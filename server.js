9b7d8c651ebc80c15d430ebdd8f315e22002bd1c
Archakov Dennis <amonbower@gmail.com>
2020-08-28 19:06:17 +0300

    Сделал запуск json-server из файла server.js, чтобы настроить деплой для Heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
+
+const PORT = process.env.port || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
